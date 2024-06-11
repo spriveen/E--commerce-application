f931b24a0a006d05d23d54e18a94665dc302ab5d
spriveen <rpeanuka@gmail.com>
2024-06-11 02:29:21 -0700

    update newletter

---
+++ frontend/src/Components/Offers/Offers.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Offers.css'
+import exclusive_image from '../Assets/exclusive_image.png'
+
+const Offers = () => {
+  return (
+    <div className='Offers'>
+      <div className="offers-left">
+        <h1>Exclusive</h1>
+        <h1>Offers For You</h1>
+        <p>ONLY ON BEST SELLER PRODUCTS</p>
+        <button>Check Now</button>
+      </div>
+      <div className="offers-right">
+        <img src={exclusive_image} alt="Exclusive Offers" />
+      </div>
+    </div>
+  )
+}
+
+export default Offers;
+
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import  Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollection from '../Components/NewCollection/NewCollection'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 const shop =()=>{
 
@@ -8,7 +11,9 @@ const shop =()=>{
         <div>
             <Hero/>
             <Popular/>
-
+            <Offers/>
+            <NewCollection/>
+            <NewsLetter/>
         </div>
     )
 }
